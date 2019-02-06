e112541d7426293cf30623ee39772b23b0329e80
gregology <greg@gho.st>
2019-02-06 17:57:31 -0500

    initial commit

---
+++ lib/pages/auth.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _AuthPageState();
+  }
+}
+
+class _AuthPageState extends State<AuthPage> {
+  String _emailValue;
+  String _passwordValue;
+  bool _acceptTerms = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: Container(
+            margin: EdgeInsets.all(10.0),
+            child: ListView(
+              children: <Widget>[
+                TextField(
+                  decoration: InputDecoration(labelText: 'Email'),
+                  keyboardType: TextInputType.emailAddress,
+                  onChanged: (String value) {
+                    setState(() {
+                      _emailValue = value;
+                    });
+                  },
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(labelText: 'Password'),
+                  onChanged: (String value) {
+                    setState(() {
+                      _passwordValue = value;
+                    });
+                  },
+                ),
+                SwitchListTile(
+                  title: Text('Accept Terms'),
+                  value: _acceptTerms,
+                  onChanged: (bool value) {
+                    setState(() {
+                      _acceptTerms = value;
+                    });
+                  },
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                RaisedButton(
+                  child: Text('Login'),
+                  color: Theme.of(context).primaryColor,
+                  textColor: Colors.white,
+                  onPressed: () {
+                    print(_emailValue);
+                    print(_passwordValue);
+                    Navigator.pushReplacementNamed(context, '/products');
+                  },
+                )
+              ],
+            )));
+  }
+}
---
+++ lib/pages/product.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+class ProductPage extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+
+  ProductPage(this.title, this.imageUrl);
+
+  _showWarningDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+              title: Text('Are you sure?'),
+              content: Text('This action cannot be undone!'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Continue'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                    Navigator.pop(context, true);
+                  },
+                ),
+                FlatButton(
+                  child: Text('Disregard'),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                )
+              ]);
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+        onWillPop: () {
+          print('Back Button Pressed!');
+          Navigator.pop(context, false);
+          return Future.value(false);
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+          ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset(imageUrl),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(title),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  color: Theme.of(context).accentColor,
+                  child: Text('DELETE'),
+                  onPressed: () => _showWarningDialog(context),
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
